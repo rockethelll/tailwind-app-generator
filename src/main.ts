2ee7056efa27bc0c68944a3076be3ffedc8c83c1
rockethelll <rockethell@hotmail.com>
2025-03-04 15:40:46 +0100

    enable to ask openai a task and display it on the page

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,44 @@
-// const form = document.getElementById('generate-form') as HTMLFormElement;
-// i want to get the form element by its id without using document
-const form = generate-form
+import { openai } from './openai';
 
-console.log(form);
+const form = document.getElementById('generate-form') as HTMLFormElement;
+const iframe = document.getElementById('generated-code') as HTMLIFrameElement;
 
-form?.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
   const formData = new FormData(form);
-  const name = formData.get('first_name');
-  alert(name);
+  const prompt = formData.get('prompt') as string;
+
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You create websites with tailwind css. You are a professional designer and developer. Your task is to create a website based on the user prompt. You will return only HTML code without any other text. You will return valid HTML. You never add any markdown.',
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+
+  const code = chatCompletion.choices[0].message.content;
+
+  if (!code) return;
+
+  iframe.srcdoc = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Generated Code</title>
+      <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    </head>
+    <body>
+      ${code}
+    </body>
+  </html>
+  `;
 });
